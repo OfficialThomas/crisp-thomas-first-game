eb653139c99c4fdd3059ea2532a5ba358f332d26
Thomas Noell <tnoell@ucsc.edu>
2022-10-04 22:09:08 -0700

    actual snow game

--- main.js
+++ main.js
@@ -1,48 +1,118 @@
-title = "JUMP STAR";
+title = "Snowy Day";
 
 description = `
-[Hold] Set angle
-[Release] Jump
+AVOID 
+HYPOTHERMIA
+
+[Hold] 
+Pause Snow
 `;
 
-characters = [];
+characters = [
+  `
+llllll
+ll l l
+ll l l
+llllll
+ l  l
+ l  l
+  `,
+  `
+llllll
+ll l l
+ll l l
+llllll
+ll  ll
+  `
+];
 
 const G = {
-  WIDTH: 200,
+  WIDTH: 100,
   HEIGHT: 150,
   STAR_SPEED_MIN: 0.5,
   STAR_SPEED_MAX: 1.0
 };
 
 options = {
-  viewSize: {x: G.WIDTH, y: G.HEIGHT}
+  viewSize: {x: G.WIDTH, y: G.HEIGHT},
+  isPlayingBgm: true,
+  isReplayEnabled: true,
+  isDrawingScoreFront: true,
+  theme: "dark"
 };
 
 /**
- * @typedef {{
- * pos: Vector,
- * speed: number
- * }} Star
- */
-
+* @typedef {{
+* pos: Vector, speed: number
+* }} Star
+*/
+  
 /**
- * @type { Star [] }
- */
-let stars;
+* @type  { Star [] }
+*/
+let stars = [];
+startick = 0;
+
+/** @type {{pos: Vector, vx: number, ty: number}} */
+let player;
 
 function update() {
   //startup function
   if (!ticks) {
-    stars = times(20, () => {
-      const posX = rnd(0, G.WIDTH);
-      const posY = rnd(0, G.HEIGHT);
+    //star setup
+    for (let i = 0; i < 2; i++) {
+      stars.push({
+          pos: vec(rnd(0, G.WIDTH), 0),
+          speed: rnd(G.STAR_SPEED_MIN, G.STAR_SPEED_MAX)
+      });
+    }
 
-      return {
-        pos: vec(posX, posY),
-        speed: rnd(0.5, 1.0)
-      };
+    player = { pos: vec(40, 98), vx: 1, ty: 90 };
+  }
+
+  // Update for Star
+  stars.forEach((s) => {
+    // Move the star downwards
+    s.pos.y += s.speed;
+    // Bring the star back to top once it's past the bottom of the screen
+    s.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
+    if (s.pos.y < G.HEIGHT && s.pos.y > 15 * G.HEIGHT/16) {
+      s.pos.x = rnd(1, G.WIDTH - 1);
+    }
+
+    // Choose a color to draw
+    color("light_black");
+    // Draw the star as a square of size 1
+    box(s.pos, 2);
+  });
+
+  startick += 1;
+  if (startick > 120) {
+    stars.push({
+      pos: vec(rnd(0, G.WIDTH), 0),
+      speed: rnd(G.STAR_SPEED_MIN, G.STAR_SPEED_MAX)
+    });
+    startick = 0;
+  }
+  
+  if (input.isPressed) {
+    stars.forEach((s) => {
+      s.speed = 0;
     });
   }
+  else {
+    stars.forEach((s) => {
+      s.speed = rnd(G.STAR_SPEED_MIN, G.STAR_SPEED_MAX);
+    });
+  }
+
+  color("light_black");
+  rect(0, 100, G.WIDTH, 200);
+
+  color("red")
+  const c = char(addWithCharCode("a", floor(ticks / 15) % 2), player.pos, {
+    mirror: { x: player.vx < 0 ? -1 : 1 },
+  }).isColliding;
 }
 
 addEventListener("load", onLoad);
